7eaa81fc7b710a79bab606bae4dceed198ba82ce
ikram-mever-codes <ikram0pakitan@gmail.com>
2024-10-17 06:05:06 +0500

    S

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
